bb59131d16c1f4c9254ff6f81116915955e6016d
ElenaSei <seimenova.elena@gmail.com>
2018-07-04 14:34:20 +0300

    Arrays

---
+++ Arrays/Lab/02. Reverse Array Of Integers/02ReverseArrayOfIntegers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02ReverseArrayOfIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int[] arr = new int[lenght];
+
+            for (int i = 0; i < lenght; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            // вариант без втори цикъл:
+            // int [] reversed = arr.Reverse().ToArray();
+            // Console.WriteLine(string.Join(" ", reversed));
+
+            // трети вариант:
+            // Array.Reverse(arr);
+            // ConsoleWriteLine(string.Join(" ", arr));
+
+        }
+    }
+}
